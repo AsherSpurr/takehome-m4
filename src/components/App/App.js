8fb5dacb8931b59a2b310eea24dbee05d3d94bec
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-03 13:04:31 -0600

    Add Home view/component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
-
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Routes>
+        <Route to='/' Component={<Home />}/>
+      </Routes>
     </div>
   );
 }
