a09e8a44a1016d9b5dc82c6c0b0d02e04e5161bc
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-05-03 14:12:54 -0600

    Cards in grid formation

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -1,20 +1,32 @@
 import MuiCard from "../MuiCard/MuiCard";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
 function Article({ articleData }) {
-  const cards = articleData.map((article) => {
-    return (
-      <MuiCard
-        image={article.urlToImage}
-        description={article.description}
-        title={article.title}
-      />
-    )
-  })
-    return (
-      <div className="Article">
-        {cards}
-      </div>
-    );
-  }
-  
-  export default Article;
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+  }));
+
+  // const cards = articleData.map((article) => {
+  //   return (
+  //     <MuiCard
+  //       image={article.urlToImage}
+  //       description={article.description}
+  //       title={article.title}
+  //     />
+  //   );
+  // });
+
+  return (
+    <div className="Article">
+      <MuiCard articleData={articleData} />
+    </div>
+  );
+}
+
+export default Article;
--- src/components/MuiCard/MuiCard.js
+++ src/components/MuiCard/MuiCard.js
@@ -1,32 +1,43 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CardActions from '@mui/material/CardActions';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CardActions from "@mui/material/CardActions";
+import Grid from "@mui/material/Grid";
 
-function MuiCard({ image, description, title  }) {
-    return (
-      <div className="MuiCard">
-        <Card>
-          <CardMedia
-            sx={{ height: 140 }}
-            image={image}
-            title="title"/>
-          <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-         {description}
-        </Typography>
-          </CardContent>
-          <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-        </Card>
-      </div>
-    );
-  }
-  
-  export default MuiCard;
+function MuiCard({ articleData }) {
+
+  return (
+    <div className="MuiCard">
+      <Grid container spacing={1} columns={3}>
+        {articleData.map((article) => {
+          return (
+            <Grid item xs={1} md={1} lg={1} xl={1}>
+              <Card>
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image={article.urlToImage}
+                  title="title"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {article.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {article.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
+}
+
+export default MuiCard;
